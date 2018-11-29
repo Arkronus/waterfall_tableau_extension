651008532c7a389e39a3b357774b8e1879744b71
Artem <aprytkov@gmail.com>
2018-11-29 21:58:10 +0300

    "Minor refactoring"

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-function loadWaterfall(waterfallData) {
+$(document).ready(function () {
+    tableau.extensions.initializeAsync().then(function () {
+      updateWaterfall();
+      addListeners();
+    }, function (err) {
+      // Something went wrong in initialization.
+      console.log('Error while Initializing: ' + err.toString());
+    });
+});
+
+function buildWaterfall(waterfallData) {
 
     var chart = new CanvasJS.Chart("chartContainer", {
     theme: "light1", // "light1", "light2", "dark1", "dark2"
@@ -24,7 +34,44 @@ function loadWaterfall(waterfallData) {
     chart.render();
 }
 
+function updateWaterfall(){
+    const worksheet = getWorksheetByName("DataSource");
+    worksheet.getSummaryDataAsync().then(function(sheetdata){
+        worksheetData = sheetdata.data.reverse();
+        var accum = []
+        worksheetData.forEach(function(row){
+            accum.push( row.map(function(row){
+                return row["_formattedValue"];            
+                })
+            );
+        })
+        const groupedData = _.groupBy(accum, row => row[1])
+        var processedData = [];
+        for (var i in groupedData){
+            processedData.push({label: i, 
+                                y: parseNumber(searchInArray(groupedData[i], "Сумма")),
+                                isIntermediateSum: parseInt(searchInArray(groupedData[i], "Пром. сумма")) === 1, 
+                                isCumulativeSum: parseInt(searchInArray(groupedData[i], "Накоп. сумма")) === 1})
+        }
 
+        buildWaterfall(processedData);
+    },
+
+    function(err){
+        console.log(err);
+    });
+}
+
+
+function addListeners() {
+    const worksheet = getWorksheetByName("DataSource");
+    const filterChanged = tableau.TableauEventType.FilterChanged;
+
+    worksheet.addEventListener(filterChanged, function (selectionEvent){
+      // When the selection changes, reload the data
+        updateWaterfall();
+    });
+}
 
 // utility functions
 
@@ -40,90 +87,13 @@ function parseNumber(numberAsString){
     return parseFloat(trimmedString)
 }
 
-function updateWaterfall(){
-
-    const dashboard = tableau.extensions.dashboardContent.dashboard;
-    const worksheets = dashboard.worksheets;
-    var processedData = [];
-
-    var readyData;
-    var worksheet = worksheets.find(function (sheet) {
-      return sheet.name === "DataSource";
-    });
-
-
-    worksheet.getSummaryDataAsync().then(function(sheetdata){
-        worksheetData = sheetdata.data.reverse();
-        var accum = []
-        worksheetData.forEach(function(row){
-            accum.push( row.map(function(row){
-                return row["_formattedValue"];            
-                })
-            );
-        })
-        const groupedData = _.groupBy(accum, row => row[1])
-        for (var i in groupedData){
-            processedData.push({label: i, 
-                                y: parseNumber(searchInArray(groupedData[i], "Сумма")),
-                                isIntermediateSum: parseInt(searchInArray(groupedData[i], "Пром. сумма")) === 1, 
-                                isCumulativeSum: parseInt(searchInArray(groupedData[i], "Накоп. сумма")) === 1})
-        }
-
-        console.log("processedData");
-        console.log(processedData);
-        loadWaterfall(processedData);
-
-    },
-    function(err){
-        console.log(err);
-    });
-}
-
-
-function printData(){
+function getWorksheetByName(sheetName){
     const dashboard = tableau.extensions.dashboardContent.dashboard;
     const worksheets = dashboard.worksheets;
 
     var worksheet = worksheets.find(function (sheet) {
-      return sheet.name === "DataSource";
+      return sheet.name === "sheetName";
     });
 
-    worksheet.getSummaryDataAsync().then(function (sumdata) {
-        // $("#groupedData").html("")
-        const worksheetData = sumdata.data.reverse();
-        const readyData = prepareData(worksheetData);
-        $("#groupedData").html(JSON.stringify(readyData));
-    });
-}
-
-function addListeners() {
-    const dashboard = tableau.extensions.dashboardContent.dashboard;
-    const worksheets = dashboard.worksheets;
-
-    var worksheet = worksheets.find(function (sheet) {
-      return sheet.name === "DataSource";
-    });
-
-    const filterChanged = tableau.TableauEventType.FilterChanged;
-
-    worksheet.addEventListener(filterChanged, function (selectionEvent){
-      // When the selection changes, reload the data
-        updateWaterfall();
-    });
-}
-
-$(document).ready(function () {
-    tableau.extensions.initializeAsync().then(function () {
-      updateWaterfall();
-      // printData();
-      addListeners();
-    }, function (err) {
-      // Something went wrong in initialization.
-      console.log('Error while Initializing: ' + err.toString());
-    });
-});
-
-
-
-
-
+    return worksheet;
+};
