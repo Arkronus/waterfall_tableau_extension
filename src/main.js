089ff0d745ce4a6584ff5ff284bca9255d8d7717
Artem <aprytkov@gmail.com>
2018-11-29 21:46:32 +0300

    Working alpha version

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-function loadWaterfall () {
+function loadWaterfall(waterfallData) {
+
     var chart = new CanvasJS.Chart("chartContainer", {
     theme: "light1", // "light1", "light2", "dark1", "dark2"
     animationEnabled: true,
@@ -17,20 +18,82 @@ function loadWaterfall () {
         indexLabelFontColor: "#EEEEEE",
         indexLabelPlacement: "inside",
         yValueFormatString: "#,##0k",
-        dataPoints: [
-            { label: "Продажи",  y: 1273 },
-            { label: "Услуги", y: 623 },
-            { label: "Общая выручка", isIntermediateSum: true, color: 'green'},
-            { label: "Исследования", y: -150 },
-            { label: "Маркетинг",  y: -226 },
-            { label: "Зарплата", y: -632 },
-            { label: "Операционная прибыль", isCumulativeSum: true },
-            { label: "Налоги",  y: -264 },
-            { label: "Чистая прибыль",  isCumulativeSum: true, color: 'green' }
-        ]
+        dataPoints: waterfallData
         }]
     });
     chart.render();
+}
+
+
+
+// utility functions
+
+function searchInArray(array, value){
+    const reqPosition = array.find(function(val){
+        return val[0] === value
+    })
+    return reqPosition[2] || 0
+}
+
+function parseNumber(numberAsString){
+    const trimmedString = numberAsString.replace(/\s/g, '')
+    return parseFloat(trimmedString)
+}
+
+function updateWaterfall(){
+
+    const dashboard = tableau.extensions.dashboardContent.dashboard;
+    const worksheets = dashboard.worksheets;
+    var processedData = [];
+
+    var readyData;
+    var worksheet = worksheets.find(function (sheet) {
+      return sheet.name === "DataSource";
+    });
+
+
+    worksheet.getSummaryDataAsync().then(function(sheetdata){
+        worksheetData = sheetdata.data.reverse();
+        var accum = []
+        worksheetData.forEach(function(row){
+            accum.push( row.map(function(row){
+                return row["_formattedValue"];            
+                })
+            );
+        })
+        const groupedData = _.groupBy(accum, row => row[1])
+        for (var i in groupedData){
+            processedData.push({label: i, 
+                                y: parseNumber(searchInArray(groupedData[i], "Сумма")),
+                                isIntermediateSum: parseInt(searchInArray(groupedData[i], "Пром. сумма")) === 1, 
+                                isCumulativeSum: parseInt(searchInArray(groupedData[i], "Накоп. сумма")) === 1})
+        }
+
+        console.log("processedData");
+        console.log(processedData);
+        loadWaterfall(processedData);
+
+    },
+    function(err){
+        console.log(err);
+    });
+}
+
+
+function printData(){
+    const dashboard = tableau.extensions.dashboardContent.dashboard;
+    const worksheets = dashboard.worksheets;
+
+    var worksheet = worksheets.find(function (sheet) {
+      return sheet.name === "DataSource";
+    });
+
+    worksheet.getSummaryDataAsync().then(function (sumdata) {
+        // $("#groupedData").html("")
+        const worksheetData = sumdata.data.reverse();
+        const readyData = prepareData(worksheetData);
+        $("#groupedData").html(JSON.stringify(readyData));
+    });
 }
 
 function addListeners() {
@@ -43,69 +106,17 @@ function addListeners() {
 
     const filterChanged = tableau.TableauEventType.FilterChanged;
 
-    worksheet.addEventListener(filterChanged, function (selectionEvent) {
+    worksheet.addEventListener(filterChanged, function (selectionEvent){
       // When the selection changes, reload the data
-        printData();
-    })
-
-}
-
-function searchInArray(array, value){
-    const reqPosition = array.find(function(val){
-        return val[0] === value
-    })
-    return reqPosition[2] || 0
-}
-
-function prepareData(worksheetData){
-    var accum = []
-
-    worksheetData.forEach(function(row){
-        accum.push( row.map(function(row){
-            return row["_formattedValue"];            
-            })
-        );
-    })
-
-    const groupedData = _.groupBy(accum, row => row[1])
-    var processedData = {}
-
-    for (var i in groupedData){
-        processedData[i] = {label: i, 
-                            y: parseFloat(searchInArray(groupedData[i], "Сумма")),
-                            isIntermediateSum: parseInt(searchInArray(groupedData[i], "Пром. сумма")) === 1  , 
-                            isCumulativeSum: parseInt(searchInArray(groupedData[i], "Накоп. сумма")) === 1}
-    }
-
-    return processedData;
-
-}
-
-function printData(){
-    const dashboard = tableau.extensions.dashboardContent.dashboard;
-    const worksheets = dashboard.worksheets;
-
-    var worksheet = worksheets.find(function (sheet) {
-      return sheet.name === "DataSource";
-    });
-
-    worksheet.getSummaryDataAsync().then(function (sumdata) {
-
-        const worksheetData = sumdata.data.reverse();
-        const readyData = prepareData(worksheetData);
-        $("#groupedData").append(JSON.stringify(readyData));
-  
-
+        updateWaterfall();
     });
 }
-
-
 
 $(document).ready(function () {
     tableau.extensions.initializeAsync().then(function () {
-      loadWaterfall();
+      updateWaterfall();
+      // printData();
       addListeners();
-      printData();
     }, function (err) {
       // Something went wrong in initialization.
       console.log('Error while Initializing: ' + err.toString());
